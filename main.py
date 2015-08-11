fb02602f59b819564f0f583e75212aee87320fa2
Jerzy Spendel <jspendel@gmail.com>
2015-08-11 11:29:30 +0200

    PEP8

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-import pygame, sys, copy
+import pygame
+import sys
+import copy
 import random
 
 pygame.init()
-BLOCK_SIZE = BLOCK_W, BLOCK_H = (8,8)
-BLOCKS = BLOCKS_IN_W, BLOCKS_IN_H= 68*2,35*2
-size = width, height = (BLOCK_W*BLOCKS[0],BLOCK_H*BLOCKS[1])
+BLOCK_SIZE = BLOCK_W, BLOCK_H = (8, 8)
+BLOCKS = BLOCKS_IN_W, BLOCKS_IN_H = 68*2, 35*2
+size = width, height = (BLOCK_W*BLOCKS[0], BLOCK_H*BLOCKS[1])
 screen = pygame.display.set_mode(size)
+
 
 class ChessBoard:
     def __init__(self, screen):
         self.screen = screen
         self.cells = []
-        for i in range(BLOCKS_IN_W):
+        for _ in range(BLOCKS_IN_W):
             cel = [False]*BLOCKS_IN_H
             self.cells.append(cel)
         self._random()
@@ -19,13 +22,13 @@ class ChessBoard:
     def _random(self):
         for x in range(BLOCKS_IN_W):
             for y in range(BLOCKS_IN_H):
-                if random.choice([True,False]) == True:
+                if random.choice([True, False]):
                     self.cells[x][y] = True
 
     def draw_net(self):
-        color = (0,0,200)
+        color = (0, 0, 200)
         for i in range(BLOCKS_IN_W):
-            start_pos = (i*BLOCK_W,0)
+            start_pos = (i*BLOCK_W, 0)
             end_pos = (i*BLOCK_W, height)
             pygame.draw.line(self.screen, color, start_pos, end_pos, 1)
         for i in range(BLOCKS_IN_H):
@@ -36,22 +39,23 @@ class ChessBoard:
     def draw_cells(self):
         for x in range(BLOCKS_IN_W):
             for y in range(BLOCKS_IN_H):
-                if self.cells[x][y] == True:
+                if self.cells[x][y]:
                     xx, yy = x*BLOCK_W, y*BLOCK_H
-                    r = pygame.Rect([xx+1,yy+1,BLOCK_W-1,BLOCK_H-1])
-                    pygame.draw.rect(self.screen, (255,255,255), r)
+                    r = pygame.Rect([xx+1, yy+1, BLOCK_W-1, BLOCK_H-1])
+                    pygame.draw.rect(self.screen, (255, 255, 255), r)
 
     def border_with(self, point):
         borders = 0
-        x,y = point
-        points = ((x+1,y),(x+1, y+1), (x,y+1), (x-1,y+1),(x-1, y), (x-1,y-1),\
-                (x,y-1), (x+1, y-1))
-        for xx,yy in points:
+        x, y = point
+        points = ((x+1, y), (x+1, y+1), (x, y+1), (x-1, y+1), (x-1, y), (x-1, y-1),
+                  (x, y-1), (x+1, y-1))
+        for xx, yy in points:
             try:
                 if 0 <= xx < BLOCKS_IN_W and 0 <= yy < BLOCKS_IN_H:
-                    if self.cells[xx][yy] == True:
+                    if self.cells[xx][yy]:
                         borders += 1
-                else: continue
+                else:
+                    continue
             except IndexError:
                 pass
         return borders
@@ -60,11 +64,11 @@ class ChessBoard:
         cells = copy.deepcopy(self.cells)
         for x in range(BLOCKS_IN_W):
             for y in range(BLOCKS_IN_H):
-                borders = self.border_with((x,y))
-                if self.cells[x][y] == False:
+                borders = self.border_with((x, y))
+                if not self.cells[x][y]:
                     if borders == 3:
                         cells[x][y] = True
-                elif self.cells[x][y] == True:
+                elif self.cells[x][y]:
                     if not 2 <= borders <= 3:
                         cells[x][y] = False
         self.cells = cells
@@ -73,9 +77,10 @@ chess = ChessBoard(screen)
 i = 0
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT:
+            sys.exit()
 
-    screen.fill((0,0,0))
+    screen.fill((0, 0, 0))
     chess.draw_net()
     chess.draw_cells()
     pygame.display.flip()
